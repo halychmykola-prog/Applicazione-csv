4882e500eb9ab2dcdb220e73e22a192aa83a524b
Mykola <halych.mykola.studente@itispaleocapa.it>
2026-05-08 10:33:44 +0200

    added struct for csv

---
+++ Applicazione csv.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+struct g{
+	string ClasseToponimo;
+	string DescrizioneToponimo;
+	int numero;
+	string Subalterno;
+	int CAP;
+	int SezioneISTAT;
+	double Lat;
+	double Lon;
+	double Location;
+	
+
+	
+};
+
+int main()
+{
+
+
+
+    return 0;
+}
